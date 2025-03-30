072ee20eeecee21921ca11102eca385a042f2ce8
Mayerikson <44329039+Mayerikson@users.noreply.github.com>
2025-03-30 19:17:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-
 import streamlit as st
 import pandas as pd
-
-# Carregar os dados
-file_path = '/content/recomendações.xlsx'
-df = pd.read_excel(file_path)
 
 # Função para classificar a confiança
 def classificar_confianca(confianca):
@@ -15,20 +10,29 @@ def classificar_confianca(confianca):
     else:
         return 'Baixa'
 
-# Adicionar uma coluna de classificação ao DataFrame
-df['Classificação'] = df['% de Confiança'].apply(classificar_confianca)
-
 # Interface do Streamlit
 st.title('Lineup de Bandas')
 
-# Seleção de bandas pelo usuário
-bandas_disponiveis = df[['Antecedent', 'Consequent']].stack().unique()
-bandas_selecionadas = st.multiselect('Selecione até 3 bandas:', bandas_disponiveis)
+# Permitir upload do arquivo Excel
+uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=["xlsx"])
 
-# Exibir resultados
-if bandas_selecionadas:
-    resultados = df[df['Antecedent'].isin(bandas_selecionadas) | df['Consequent'].isin(bandas_selecionadas)]
-    st.write('Bandas Selecionadas e suas Classificações de Confiança:')
-    st.dataframe(resultados[['Antecedent', 'Consequent', '% de Confiança', 'Classificação']])
+if uploaded_file is not None:
+    # Carregar os dados do arquivo Excel
+    df = pd.read_excel(uploaded_file)
+
+    # Adicionar uma coluna de classificação ao DataFrame
+    df['Classificação'] = df['% de Confiança'].apply(classificar_confianca)
+
+    # Seleção de bandas pelo usuário
+    bandas_disponiveis = df[['Antecedent', 'Consequent']].stack().unique()
+    bandas_selecionadas = st.multiselect('Selecione até 3 bandas:', bandas_disponiveis)
+
+    # Exibir resultados
+    if bandas_selecionadas:
+        resultados = df[df['Antecedent'].isin(bandas_selecionadas) | df['Consequent'].isin(bandas_selecionadas)]
+        st.write('Bandas Selecionadas e suas Classificações de Confiança:')
+        st.dataframe(resultados[['Antecedent', 'Consequent', '% de Confiança', 'Classificação']])
+    else:
+        st.write('Por favor, selecione até 3 bandas.')
 else:
-    st.write('Por favor, selecione até 3 bandas.')
+    st.write('Por favor, faça upload de um arquivo Excel.')
