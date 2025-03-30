0015bbdc247c85093e6e329c802c335562d26fa6
Mayerikson <44329039+Mayerikson@users.noreply.github.com>
2025-03-30 19:22:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,29 +10,24 @@ def classificar_confianca(confianca):
     else:
         return 'Baixa'
 
+# Carregar os dados do arquivo Excel
+file_path = 'recomendações.xlsx'  # Certifique-se de que o caminho está correto
+df = pd.read_excel(file_path)
+
+# Adicionar uma coluna de classificação ao DataFrame
+df['Classificação'] = df['% de Confiança'].apply(classificar_confianca)
+
 # Interface do Streamlit
 st.title('Lineup de Bandas')
 
-# Permitir upload do arquivo Excel
-uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=["xlsx"])
+# Seleção de bandas pelo usuário
+bandas_disponiveis = df[['Antecedent', 'Consequent']].stack().unique()
+bandas_selecionadas = st.multiselect('Selecione até 3 bandas:', bandas_disponiveis)
 
-if uploaded_file is not None:
-    # Carregar os dados do arquivo Excel
-    df = pd.read_excel(uploaded_file)
-
-    # Adicionar uma coluna de classificação ao DataFrame
-    df['Classificação'] = df['% de Confiança'].apply(classificar_confianca)
-
-    # Seleção de bandas pelo usuário
-    bandas_disponiveis = df[['Antecedent', 'Consequent']].stack().unique()
-    bandas_selecionadas = st.multiselect('Selecione até 3 bandas:', bandas_disponiveis)
-
-    # Exibir resultados
-    if bandas_selecionadas:
-        resultados = df[df['Antecedent'].isin(bandas_selecionadas) | df['Consequent'].isin(bandas_selecionadas)]
-        st.write('Bandas Selecionadas e suas Classificações de Confiança:')
-        st.dataframe(resultados[['Antecedent', 'Consequent', '% de Confiança', 'Classificação']])
-    else:
-        st.write('Por favor, selecione até 3 bandas.')
+# Exibir resultados
+if bandas_selecionadas:
+    resultados = df[df['Antecedent'].isin(bandas_selecionadas) | df['Consequent'].isin(bandas_selecionadas)]
+    st.write('Bandas Selecionadas e suas Classificações de Confiança:')
+    st.dataframe(resultados[['Antecedent', 'Consequent', '% de Confiança', 'Classificação']])
 else:
-    st.write('Por favor, faça upload de um arquivo Excel.')
+    st.write('Por favor, selecione até 3 bandas.')
